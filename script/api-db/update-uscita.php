143b2316ae7647ece1e34e2943a938bdcf4dd8ce
Dante <68009431+IsD4n73@users.noreply.github.com>
2023-03-02 08:48:46 +0100

    Update update-uscita.php

--- script/api-db/update-uscita.php
+++ script/api-db/update-uscita.php
@@ -2,6 +2,7 @@
 
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
